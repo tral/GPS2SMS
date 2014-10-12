a4708367f252f7eb6822b7232054a75537c70dc8
tral <tralmail@gmail.com>
2014-10-12 21:43:34 +0600

    code formatting

--- src/ru/perm/trubnikov/gps2sms/IncomingSms.java
+++ src/ru/perm/trubnikov/gps2sms/IncomingSms.java
@@ -15,54 +15,63 @@ import android.telephony.SmsMessage;
 import android.util.Log;
 
 public class IncomingSms extends BroadcastReceiver {
-    
-    // Get the object of SmsManager
-    final SmsManager sms = SmsManager.getDefault();
-     
-    public void onReceive(Context context, Intent intent) {
-     
-    	SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
-    	if (sharedPrefs.getBoolean("prefRegexpSMS", true)) {
-    	
-	        // Retrieves a map of extended data from the intent.
-	        final Bundle bundle = intent.getExtras();
-	 
-	        try {
-	             
-	            if (bundle != null) {
-	                 
-	                final Object[] pdusObj = (Object[]) bundle.get("pdus");
-	                 
-	                for (int i = 0; i < pdusObj.length; i++) {
-	                     
-	                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
-	                    //String phoneNumber = currentMessage.getDisplayOriginatingAddress();
-	                    //String senderNum = phoneNumber;
-	                    String message = currentMessage.getDisplayMessageBody();
-	                    //Log.d("gps", "senderNum: "+ senderNum + "; message: " + message);
-	                     
-	                    Pattern p = Pattern.compile("(\\-?\\d+\\.(\\d+)?),\\s*(\\-?\\d+\\.(\\d+)?)");
-	 			        Matcher m = p.matcher(message);
-	 			        
-		 			    if(m.find()) {
-		 			    	Intent intent_openmap = new Intent(Intent.ACTION_VIEW, Uri.parse("geo:"+ m.group(0)));
-		 			    	intent_openmap.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-		 			    	context.startActivity(intent_openmap);
-		 			    }
-		 			     
-	                   // Show Alert
-	                   //Toast toast = Toast.makeText(context, clip, Toast.LENGTH_LONG);
-	                   //toast.show();
-	
-	                } // end for loop
-	              } // bundle is null
-	 
-	        } catch (Exception e) {
-	            Log.d("gps", "Exception smsReceiver" +e);
-	             
-	        }
-        
-    	}
-        
-    }    
+
+	// Get the object of SmsManager
+	final SmsManager sms = SmsManager.getDefault();
+
+	public void onReceive(Context context, Intent intent) {
+
+		SharedPreferences sharedPrefs = PreferenceManager
+				.getDefaultSharedPreferences(context);
+		if (sharedPrefs.getBoolean("prefRegexpSMS", true)) {
+
+			// Retrieves a map of extended data from the intent.
+			final Bundle bundle = intent.getExtras();
+
+			try {
+
+				if (bundle != null) {
+
+					final Object[] pdusObj = (Object[]) bundle.get("pdus");
+
+					for (int i = 0; i < pdusObj.length; i++) {
+
+						SmsMessage currentMessage = SmsMessage
+								.createFromPdu((byte[]) pdusObj[i]);
+						// String phoneNumber =
+						// currentMessage.getDisplayOriginatingAddress();
+						// String senderNum = phoneNumber;
+						String message = currentMessage.getDisplayMessageBody();
+						// Log.d("gps", "senderNum: "+ senderNum + "; message: "
+						// + message);
+
+						Pattern p = Pattern
+								.compile("(\\-?\\d+\\.(\\d+)?),\\s*(\\-?\\d+\\.(\\d+)?)");
+						Matcher m = p.matcher(message);
+
+						if (m.find()) {
+							Intent intent_openmap = new Intent(
+									Intent.ACTION_VIEW, Uri.parse("geo:"
+											+ m.group(0)));
+							intent_openmap
+									.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+							context.startActivity(intent_openmap);
+						}
+
+						// Show Alert
+						// Toast toast = Toast.makeText(context, clip,
+						// Toast.LENGTH_LONG);
+						// toast.show();
+
+					} // end for loop
+				} // bundle is null
+
+			} catch (Exception e) {
+				Log.d("gps", "Exception smsReceiver" + e);
+
+			}
+
+		}
+
+	}
 }
--- src/ru/perm/trubnikov/gps2sms/UserSettingActivity.java
+++ src/ru/perm/trubnikov/gps2sms/UserSettingActivity.java
@@ -1,75 +1,70 @@
 ﻿package ru.perm.trubnikov.gps2sms;
+
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceGroup;
-import android.preference.PreferenceManager;
 import android.view.MenuItem;
 
-
-
 public class UserSettingActivity extends PreferenceActivity {
- 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-       	ShowBackButton();
-        addPreferencesFromResource(R.xml.settings);
-    
-    	this.initSummaries(this.getPreferenceScreen());
 
-    }
-    
-    
-    private void setSummary(Preference pref) {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-    	// react on type or key
-        if (pref instanceof Preference) {
-//    	      ListPreference listPref = (ListPreference) pref;
-//    	      pref.setSummary(listPref.getEntry());
-//        	  Log.d("seagull", "EXCEPTION! " + pref.getKey());
-        	  if (pref.getKey().equalsIgnoreCase("prefAbout")) {
-        		  pref.setSummary(getString(R.string.pref_about_summary) + " " + getString(R.string.version_name));        		 
-        	  }
-        }
-    }
-    
-    private void initSummaries(PreferenceGroup pg) {
-        for (int i = 0; i < pg.getPreferenceCount(); ++i) {
-        Preference p = pg.getPreference(i);
-        if (p instanceof PreferenceGroup)
-          this.initSummaries((PreferenceGroup) p); // recursion
-        else
-          this.setSummary(p);
-        }
-      }
+		ShowBackButton();
+		addPreferencesFromResource(R.xml.settings);
 
-    
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	switch (item.getItemId()) 
-    	   {        
-    	      case android.R.id.home:            
-    	         Intent intent = new Intent(this, MainActivity.class);            
-    	         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); 
-    	         startActivity(intent);            
-    	         return true;        
-    	      default:            
-    	         return super.onOptionsItemSelected(item);    
-    	   }
-    }
-    
-    @TargetApi(11)
-    public void ShowBackButton() {
-    	if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
-    		// call something for API Level 11+
-    		getActionBar().setDisplayHomeAsUpEnabled(true);
-    	}
-    }
-    
+		this.initSummaries(this.getPreferenceScreen());
+
+	}
+
+	private void setSummary(Preference pref) {
+
+		// react on type or key
+		if (pref instanceof Preference) {
+			// ListPreference listPref = (ListPreference) pref;
+			// pref.setSummary(listPref.getEntry());
+			// Log.d("seagull", "EXCEPTION! " + pref.getKey());
+			if (pref.getKey().equalsIgnoreCase("prefAbout")) {
+				pref.setSummary(getString(R.string.pref_about_summary) + " "
+						+ getString(R.string.version_name));
+			}
+		}
+	}
+
+	private void initSummaries(PreferenceGroup pg) {
+		for (int i = 0; i < pg.getPreferenceCount(); ++i) {
+			Preference p = pg.getPreference(i);
+			if (p instanceof PreferenceGroup)
+				this.initSummaries((PreferenceGroup) p); // recursion
+			else
+				this.setSummary(p);
+		}
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			Intent intent = new Intent(this, MainActivity.class);
+			intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+	@TargetApi(11)
+	public void ShowBackButton() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			// call something for API Level 11+
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
+	}
+
 }
