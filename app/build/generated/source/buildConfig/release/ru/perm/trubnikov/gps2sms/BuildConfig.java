04ca9303ebd2e33cdfa2248e90eb34a53e630932
tralchonok <tralmail@gmail.com>
2015-01-02 19:17:39 +0530

    app/build (???)

--- app/build/generated/source/buildConfig/release/ru/perm/trubnikov/gps2sms/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/ru/perm/trubnikov/gps2sms/BuildConfig.java
@@ -8,6 +8,6 @@ public final class BuildConfig {
   public static final String APPLICATION_ID = "ru.perm.trubnikov.gps2sms";
   public static final String BUILD_TYPE = "release";
   public static final String FLAVOR = "";
-  public static final int VERSION_CODE = 55;
+  public static final int VERSION_CODE = 56;
   public static final String VERSION_NAME = "";
 }
