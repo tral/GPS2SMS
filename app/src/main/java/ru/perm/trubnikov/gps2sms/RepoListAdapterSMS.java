b73a2996772439dd655f5586bed5d54156c91292
tralchonok <tralmail@gmail.com>
2015-01-31 22:33:11 +0530

    -

--- app/src/main/java/ru/perm/trubnikov/gps2sms/RepoListAdapterSMS.java
+++ app/src/main/java/ru/perm/trubnikov/gps2sms/RepoListAdapterSMS.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
